ad9511f22e9bc12483d480f78e1b371e97cd6dd3
Rahul Horé <hore.rahul@gmail.com>
2013-12-16 15:59:13 -0500

    Continues cleaning up styling

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,6 +10,11 @@ class MainController < ApplicationController
       @peeps = @company.relationships.map do |peep|
         Crunchbase::Person.get(peep.person_permalink)
       end
+      # @pois = @peeps.map do |peep|
+      #   poi_name = peep.person_permalink.sub('-', ' ')
+      #   poi_id = AngellistApi.search(name).first.id
+      #   AngellistApi.get_user(poi_id)
+      # end
       # respond_to do |format|
       #   format.html { redirect_to root_path }
       #   format.js
