a0a3799e7f7576c328e935cb369c877a0b8e5698
Rahul Horé <hore.rahul@gmail.com>
2013-12-16 10:14:47 -0500

    Continues to dump data to page without AJAX

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,9 +7,9 @@ class MainController < ApplicationController
     unless params[:search].blank?
       # @company = Company.search(params[:search])
       @company = Crunchbase::Company.get(params[:search].split.join('-'))
-      # @peeps = @company.relationships.map do |peep|
-      #   Crunchbase::Person.get(peep.person_permalink)
-      # end
+      @peeps = @company.relationships.map do |peep|
+        Crunchbase::Person.get(peep.person_permalink)
+      end
       # respond_to do |format|
       #   format.html { redirect_to root_path }
       #   format.js
