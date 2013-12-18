14f006e435e0dcf0cfb34280c8c3788cb4d50c3b
Rahul Horé <hore.rahul@gmail.com>
2013-12-18 14:27:07 -0500

    Fixes file name error

--- db/migrate/20131217233526_create_connections.rb
+++
@@ -1,8 +0,0 @@
-class CreateAssociations < ActiveRecord::Migration
-  def change
-    create_table :associations do |t|
-      t.belongs_to :person, index: true
-      t.belongs_to :associate, index: true
-    end
-  end
-end
