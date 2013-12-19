fc738bf7342a08c16ddf92463b5c1bbfcbcccf33
Rahul Horé <hore.rahul@gmail.com>
2013-12-19 10:39:56 -0500

    Adds flash alert for when search term not found

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,13 +5,22 @@ class MainController < ApplicationController
 
   def search
     unless params[:search].blank?
-      # @company = Company.search(params[:search])
-      search_term = params[:search].split.join('-').gsub('.', '-')
-      @company = Company.where(permalink: search_term)
-      Crunchbase::Company.get(search_term) if @company.empty?
-      @people = @company.relationships.map do |person|
-        Crunchbase::Person.get(person.person_permalink)
+      begin
+        search_term = params[:search].split.join('-').gsub('.', '-')
+        @company = Crunchbase::Company.get(search_term)
+        @people = @company.relationships.map do |person|
+          Crunchbase::Person.get(person.person_permalink)
+        end
+      rescue
+        redirect_to root_path, alert: "Sorry, we couldn't find anything on `#{params[:search]}`"
       end
+      # @company = Company.search(params[:search])
+      # search_term = params[:search].split.join('-').gsub('.', '-')
+      # @company = Company.where(permalink: search_term)
+      # Crunchbase::Company.get(search_term) if @company.empty?
+      # @people = @company.relationships.map do |person|
+      #   Crunchbase::Person.get(person.person_permalink)
+      # end
       # @pois = @peeps.map do |peep|
       #   poi_name = peep.person_permalink.sub('-', ' ')
       #   poi_id = AngellistApi.search(name).first.id
