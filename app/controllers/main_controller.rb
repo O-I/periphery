7594dc4e8342b4ee2aa46ff2c2b20df9060f6b19
Rahul Horé <hore.rahul@gmail.com>
2013-12-25 17:27:28 -0500

    Adds one more general TODO in main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,7 @@ class MainController < ApplicationController
   # TODO; Needs major DRYing up and separation of concerns
   # TODO; Consider using Redis caching instead of a database
   # TODO; Figure out how to pull down photos from other sources
+  # TODO; Clean up the database; view logic is getting out of hand
 
   def search
     unless params[:search].blank?
